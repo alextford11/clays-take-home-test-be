73c414280514b50571a61b07b560dc19ef586852
Alex Ford <alextford1@gmail.com>
2024-06-26 18:05:44 +0100

    changes

--- clays_take_home_test/settings.py
+++ clays_take_home_test/settings.py
@@ -16,6 +16,9 @@ SECRET_KEY = 'django-insecure-ywglg(2f90gl=jc-38!ivduhp6_)ill+&f0yy6r$m-3#f+0d=3
 DEBUG = True
 
 ALLOWED_HOSTS = []
+CORS_ALLOWED_ORIGINS = [
+    'http://localhost:3000',
+]
 
 
 # Application definition
@@ -28,12 +31,14 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
     'rest_framework',
+    'corsheaders',
     'notes.apps.NotesConfig',
 ]
 
 MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
+    'corsheaders.middleware.CorsMiddleware',
     'django.middleware.common.CommonMiddleware',
     'django.middleware.csrf.CsrfViewMiddleware',
     'django.contrib.auth.middleware.AuthenticationMiddleware',
